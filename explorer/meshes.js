02ce3bf6f04ebde8fc8d4f73739e4d627635c169
Florian Mounier <paradoxxx.zero@gmail.com>
2024-02-10 14:42:55 +0100

    Mesh -> mesh

--- explorer/meshes.js
+++ explorer/meshes.js
@@ -1,8 +1,9 @@
 import { types } from '../statics'
+import { range } from '../utils'
 import { sphere, tri, tube } from './geometries'
-import { mesh } from './helpers'
+import { attribute, augment, indices, compileProgram } from './helpers'
 import { PI, tan } from './math'
-import { columnMajor } from './math/matrix'
+import { columnMajor, ident } from './math/matrix'
 import { render } from './render'
 import fragmentEdge from './shaders/edge/fragment.glsl?raw'
 import vertexEdge from './shaders/edge/vertex.glsl?raw'
@@ -10,6 +11,7 @@ import fragmentFace from './shaders/face/fragment.glsl?raw'
 import vertexFace from './shaders/face/vertex.glsl?raw'
 import fragmentVertex from './shaders/vertex/fragment.glsl?raw'
 import vertexVertex from './shaders/vertex/vertex.glsl?raw'
+import { getArity } from './utils'
 
 const geometries = {
   vertex: (_, detail) =>
@@ -54,6 +56,218 @@ const geometries = {
         ultra: { segments: curve ? 128 : 1 },
       }[detail || 'medium']
     ),
+}
+
+export const mesh = (
+  rt,
+  type,
+  vertex,
+  fragment,
+  geometryFunc,
+  size,
+  arity,
+  varying = ['position']
+) => {
+  const { gl } = rt
+  const geometry = geometryFunc(rt.space?.curvature && rt.curve, rt.detail)
+  const uniforms = rt => [
+    {
+      name: 'viewProjection',
+      type: 'm4fv',
+      value: columnMajor(ident(4)),
+    },
+    {
+      name: 'matrix',
+      type: `m${rt.dimensions}fv`,
+      value: columnMajor(rt.matrix),
+    },
+    {
+      name: 'eye',
+      type: `3fv`,
+      value: [0, 0, 0],
+    },
+    {
+      name: 'metric',
+      type: `m${rt.dimensions}fv`,
+      value: columnMajor(ident(rt.dimensions)),
+    },
+    {
+      name: 'zoom',
+      type: '1f',
+      value: 1,
+    },
+    {
+      name: 'envMap',
+      type: '1i',
+      value: 3,
+    },
+    {
+      name: 'textureMap',
+      type: '1i',
+      value: 4,
+    },
+    {
+      name: 'normalMap',
+      type: '1i',
+      value: 5,
+    },
+    {
+      name: 'displacementMap',
+      type: '1i',
+      value: 6,
+    },
+    ...(['vertex', 'edge'].includes(type)
+      ? [
+          {
+            name: 'thickness',
+            type: '1f',
+            value: 0,
+          },
+        ]
+      : []),
+    ...range(3, rt.dimensions + 1, 1, true).map(i => ({
+      name: `fov${i}`,
+      type: '1f',
+      value: rt[`fov${i}`],
+    })),
+  ]
+
+  arity = getArity(arity)
+  const mesh = {
+    attributes: {},
+    varying,
+    vertex,
+    type,
+    fragment,
+    ...compileProgram(
+      rt,
+      type,
+      ...augment(rt, vertex, fragment, type),
+      uniforms(rt)
+    ),
+    recompileProgram(rt) {
+      const [newVertex, newFragment] = augment(
+        rt,
+        this.vertex,
+        this.fragment,
+        this.type
+      )
+      this.recompile(rt, newVertex, newFragment, uniforms(rt))
+    },
+    changeArity(arity) {
+      arity = getArity(arity)
+      if (this.arity === arity) {
+        return
+      }
+      this.arity = arity
+      this.extendAttributes(this.instances)
+    },
+    extendAttributes(maxSize) {
+      this.instances = maxSize
+      varying.forEach(attr => {
+        this.attributes[attr].extend(
+          this.arity,
+          new Float32Array(maxSize * this.arity),
+          true
+        )
+      })
+      this.attributes.color.extend(3, new Float32Array(maxSize * 3), true)
+    },
+    updateGeometry(rt) {
+      const geometry = geometryFunc(rt.space.curvature && rt.curve, rt.detail)
+      this.indices.update(new Uint16Array(geometry.indices))
+      this.attributes.vertex.extend(3, new Float32Array(geometry.vertices))
+      this.attributes.uv.extend(2, new Float32Array(geometry.uvs))
+      this.attributes.normal.extend(3, new Float32Array(geometry.normals))
+    },
+    fillGeometry(data, info) {
+      if (info.arity !== this.arity) {
+        return
+      }
+      this.count = info.start + info.size
+      if (this.instances < this.count) {
+        this.extendAttributes(this.count)
+      }
+
+      for (let i = 0; i < this.varying.length; i++) {
+        const attr = this.varying[i]
+        this.attributes[attr].update(data[i], info.start, info.size)
+      }
+    },
+    fillColor(data, info) {
+      if (info.arity !== this.arity) {
+        return
+      }
+      this.count = info.start + info.size
+      if (this.instances < this.count) {
+        this.extendAttributes(this.count)
+      }
+
+      this.attributes.color.update(data, info.start, info.size)
+    },
+    render(rt) {
+      if (!this.count) {
+        return
+      }
+      const { gl } = rt
+      gl.useProgram(this.program)
+      gl.bindVertexArray(this.vao)
+      gl.drawElementsInstanced(
+        gl.TRIANGLES,
+        this.indices.count,
+        gl.UNSIGNED_SHORT,
+        0,
+        this.count
+      )
+    },
+  }
+  mesh.vao = gl.createVertexArray()
+  mesh.indices = indices(rt, mesh.vao, new Uint16Array(geometry.indices))
+
+  mesh.attributes.vertex = attribute(
+    rt,
+    mesh,
+    'vertex',
+    3,
+    new Float32Array(geometry.vertices)
+  )
+  mesh.attributes.uv = attribute(
+    rt,
+    mesh,
+    'uv',
+    2,
+    new Float32Array(geometry.uvs)
+  )
+  mesh.attributes.normal = attribute(
+    rt,
+    mesh,
+    'normal',
+    3,
+    new Float32Array(geometry.normals)
+  )
+  mesh.attributes.color = attribute(
+    rt,
+    mesh,
+    'color',
+    3,
+    new Float32Array(size * 3),
+    1
+  )
+  varying.forEach(attr => {
+    mesh.attributes[attr] = attribute(
+      rt,
+      mesh,
+      attr,
+      arity,
+      new Float32Array(size * arity),
+      1
+    )
+  })
+  mesh.type = type
+  mesh.arity = arity
+  mesh.instances = size
+  mesh.count = 0
+  return { [type]: mesh }
 }
 
 export default function getMeshes(rt) {
